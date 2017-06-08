6b69927f4ba3154ad33e765a51082a8c5fcfa413
S <s>
2017-06-08 07:23:10 +0100

    {"types":"C","runs":17}

--- Breakout.java
+++ Breakout.java
@@ -23,14 +23,9 @@ public class Breakout extends GraphicsProgram {
 
 /** Dimensions of game board (usually the same) */
 	private static final int WIDTH = APPLICATION_WIDTH;
-	private static final int HEIGHT = APPLICATION_HEIGHT;
-
-/** Dimensions of the paddle */
+	/** Dimensions of the paddle */
 	private static final int PADDLE_WIDTH = 60;
 	private static final int PADDLE_HEIGHT = 10;
-
-/** Offset of the paddle up from the bottom */
-	private static final int PADDLE_Y_OFFSET = 30;
 
 /** Number of bricks per row */
 	private static final int NBRICKS_PER_ROW = 10;
@@ -48,14 +43,10 @@ public class Breakout extends GraphicsProgram {
 /** Height of a brick */
 	private static final int BRICK_HEIGHT = 8;
 
-/** Radius of the ball in pixels */
-	private static final int BALL_RADIUS = 10;
-
 /** Offset of the top brick row from the top */
 	private static final int BRICK_Y_OFFSET = 70;
 
-/** Number of turns */
-	private static final int NTURNS = 3;
+
 
 /* Method: run() */
 	
@@ -133,7 +124,7 @@ double x = 0.5;
 	}
 	private GObject gobj;
 	private GPoint last;
-	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 }
 		
 	
