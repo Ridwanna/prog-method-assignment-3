60d95eb634f2507000c12d95c9b0832f6258a54e
S <s>
2017-06-08 14:35:57 +0100

    {"types":"C","runs":116}

--- Breakout.java
+++ Breakout.java
@@ -95,7 +95,7 @@ private double PADDLE_WIDHT;
 		
 		//setup Paddle
 			int i = ((getWidth() /2) - PADDLE_WIDTH);
-			int k = ((getHeight() /2) - PADDLE_HEIGHT) * 2;
+			int k = ((getHeight() /2) - PADDLE_HEIGHT) * 1;
 			
 			//insert paddle
 			GRect paddle = new GRect (i, k, PADDLE_WIDTH, PADDLE_HEIGHT);
