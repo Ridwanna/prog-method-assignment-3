7801be0fd664a094da11156881d408fc72fd9d0b
S <s>
2017-06-08 07:58:31 +0100

    {"types":"C","runs":40}

--- Breakout.java
+++ Breakout.java
@@ -99,7 +99,7 @@ double x = 0.5;
 	public void init() {
 		addMouseListeners();
 		double x = ((getWidth() /2)-PADDLE_WIDTH);
-		double y = ((getHeight() /2)-PADDLE_HEIGHT * (-20));
+		double y = ((getHeight() /2)-PADDLE_HEIGHT * (-23));
 		GRect mousePaddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
 		mousePaddle.setFillColor(Color.GRAY);
 		mousePaddle.setFilled(true);
