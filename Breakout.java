b3a8b138045cec8a308414dca9ec79446720e42b
S <s>
2017-06-08 11:24:04 +0100

    {"types":"C","runs":56}

--- Breakout.java
+++ Breakout.java
@@ -71,7 +71,7 @@ public class Breakout extends GraphicsProgram {
 	
 	
 		private void getRow(double x, double y, int bricks) {
-			for (int i = 0; i < bricks; i++) {
+			for (int i = 0; i < NBRICK_ROWS; i++) {
 				getBrick((x + i * BRICK_WIDTH), y);
 				x += BRICK_SEP;
 				
