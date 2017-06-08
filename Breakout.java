c879b6ffe1910bb175a745a35fd83cf4c6f6075f
S <s>
2017-06-08 22:40:20 +0100

    {"types":"C","runs":134}

--- Breakout.java
+++ Breakout.java
@@ -17,40 +17,50 @@ import java.awt.event.*;
 
 public class Breakout extends GraphicsProgram {
 
-/** Width and height of application window in pixels */
-	public static final int APPLICATION_WIDTH = 400;
-	public static final int APPLICATION_HEIGHT = 600;
-
+	/** Width and height of application window in pixels */
+    public static final int APPLICATION_WIDTH = 400;
+    public static final int APPLICATION_HEIGHT = 600;
+ 
 /** Dimensions of game board (usually the same) */
-	private static final int WIDTH = APPLICATION_WIDTH;
-	/** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 60;
-	private static final int PADDLE_HEIGHT = 10;
-
+    private static final int WIDTH = APPLICATION_WIDTH;
+    private static final int HEIGHT = APPLICATION_HEIGHT;
+ 
+/** Dimensions of the paddle */
+    private static final int PADDLE_WIDTH = 60;
+    private static final int PADDLE_HEIGHT = 10;
+ 
+/** Offset of the paddle up from the bottom */
+    private static final int PADDLE_Y_OFFSET = 30;
+ 
 /** Number of bricks per row */
-	private static final int NBRICKS_PER_ROW = 10;
-
+    private static final int NBRICKS_PER_ROW = 10;
+ 
 /** Number of rows of bricks */
-	private static final int NBRICK_ROWS = 10;
-
+    private static final int NBRICK_ROWS = 10;
+     
+/** Total number of bricks */
+    private static final int TOTAL_BRICKS = NBRICK_ROWS * NBRICKS_PER_ROW;
+ 
 /** Separation between bricks */
-	private static final int BRICK_SEP = 4;
-
+    private static final int BRICK_SEP = 4;
+ 
 /** Width of a brick */
-	private static final int BRICK_WIDTH =
-	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
-
+    private static final int BRICK_WIDTH =
+      (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+ 
 /** Height of a brick */
-	private static final int BRICK_HEIGHT = 8;
-
+    private static final int BRICK_HEIGHT = 8;
+ 
+/** Radius of the ball in pixels */
+    private static final int BALL_RADIUS = 10;
+ 
 /** Offset of the top brick row from the top */
-	private static final int BRICK_Y_OFFSET = 70;
-	
-	//
-	private static final int PADDLE_Y_OFFSET = 30;
-
-
-
+    private static final int BRICK_Y_OFFSET = 70;
+ 
+/** Number of turns */
+    private static final int NTURNS = 3;
+private Component getPaddle;
+ 
 
 /* Method: run() */
 	
@@ -58,7 +68,7 @@ public class Breakout extends GraphicsProgram {
 	
 /** Runs the Breakout program. */
 	public void run() {
-		createPaddle();
+		createBricks();
 		getPaddle();
 		
 }
@@ -66,7 +76,7 @@ public class Breakout extends GraphicsProgram {
  
 
 
-		private void createPaddle() {
+		private void createBricks() {
 		
 			//the following program creates bricks
 			
@@ -129,5 +139,17 @@ public class Breakout extends GraphicsProgram {
 			addMouseListeners();
 			}
 
+		
+		// makes paddle move
+	    public void mouseMoved(MouseEvent e)	{
+	    	
+	    	getPaddle.setLocation(e.getX() - PADDLE_WIDTH/2, getPaddle.getY());
+	        if (getPaddle.getX() <= 0) getPaddle.setLocation(0, getPaddle.getY());
+	        if (getPaddle.getX() + PADDLE_WIDTH >= getWidth()) getPaddle.setLocation(getWidth() - PADDLE_WIDTH, getPaddle.getY());
+	    }
 }
 		
+
+
+
+
